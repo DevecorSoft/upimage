112b0863c923266b234bc06b63eb9d8767323433
devecor <devecor@outlook.com>
2021-09-17 11:29:22 +0800

    Init spring boot

---
+++ src/main/kotlin/cn/devecor/upimage/UpimageApplication.kt
@@ -0,0 +1,11 @@
+package cn.devecor.upimage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UpimageApplication
+
+fun main(args: Array<String>) {
+    runApplication<UpimageApplication>(*args)
+}
---
+++ src/test/kotlin/cn/devecor/upimage/UpimageApplicationTests.kt
@@ -0,0 +1,13 @@
+package cn.devecor.upimage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UpimageApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
