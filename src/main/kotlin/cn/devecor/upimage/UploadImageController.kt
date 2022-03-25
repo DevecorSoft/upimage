45e2ce2343bbc80f04c10c9e08d1a3a12cf78678
devecor <devecor@outlook.com>
2022-03-25 15:01:21 +0800

    release upimage to staging

--- src/main/kotlin/cn/devecor/upimage/UploadImageController.kt
+++ src/main/kotlin/cn/devecor/upimage/UploadImageController.kt
@@ -3,7 +3,6 @@ package cn.devecor.upimage
 import org.springframework.core.io.InputStreamResource
 import org.springframework.core.io.Resource
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
@@ -35,7 +34,7 @@ class UploadImageController(
     }
 
     @CrossOrigin
-    @PostMapping("/image")
+    @PostMapping(Endpoints.IMAGE)
     fun postImage(file: MultipartFile): ResponseEntity<String> {
         val url = imageStorageService.saveImage(file)
         return if (url.isNotEmpty()) {
